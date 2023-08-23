dec8e5b6ec3cd7054d478681d0114bb14fab5fc6
Hakan <mangacanmantan11@gmail.com>
2023-08-24 00:31:23 +0300

    new

--- OwnTodo/script.js
+++ OwnTodo/script.js
@@ -1,29 +1,41 @@
-todoList=[];
-function addTodo(){
-  const inputElement = document.querySelector('.input-name')
-  const name = inputElement.value;
-  const dateElement = document.querySelector('.input-date');
-  const dueDate = dateElement.value; 
-  todoList.push({
-    name, dueDate});
-    inputElement.value = '';
-    showTodoList();
+const todoList = JSON.parse(localStorage.getItem('todoList'))|| []
 
+
+  renderTodoList();
+
+
+function addTodo(){
+  const nameElement= document.querySelector('.js-name-input');
+  const name = nameElement.value;
+  todoList.push(name);
+ 
+  nameElement.value='';
+  renderTodoList();
+  saveToStorage()
 }
 
-function showTodoList(){
-  let todoListHtml= ''
+function renderTodoList() {
+  todoListHtml = '';
 
   for (let i=0; i<todoList.length; i++) {
-      const todoObject = todoList[i];
-      console.log(todoObject)
-      const {name, dueDate} = todoObject
-    html = `<div >name:${name}</div>  /* class eklenicek */
-    <div>dueDate:${dueDate}</div>
-    <button>&minus;</button>
+    const name = todoList[i];
+    const html = `<div>${name}</div>
+    <button onclick="
+    todoList.splice(${i},1)
+    renderTodoList();
+    saveToStorage();
+    " class="delete-todo-button" 
+    >Delete</button>
     `
-
     todoListHtml += html
-  }
-  document.querySelector('.js-list').innerHTML = todoListHtml
+    
+}
+document.querySelector('.js-todolist').innerHTML = todoListHtml
+}
+
+
+
+
+function saveToStorage(){
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
