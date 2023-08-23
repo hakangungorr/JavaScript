ddbe89b27c33cd943b9721f58f1f2d72cc55ba72
Hakan <mangacanmantan11@gmail.com>
2023-08-24 01:38:09 +0300

    update

--- OwnTodo/script.js
+++ OwnTodo/script.js
@@ -4,22 +4,35 @@ const todoList = JSON.parse(localStorage.getItem('todoList'))|| []
   renderTodoList();
 
 
+function pressEnter(event) {
+  const key =  event.key
+  if (key === 'Enter' || key === '=') 
+  addTodo();
+
+}
+
+
 function addTodo(){
+  
   const nameElement= document.querySelector('.js-name-input');
   const name = nameElement.value;
-  todoList.push(name);
+
+  if(name !=''){
+    todoList.push(name);
  
-  nameElement.value='';
-  renderTodoList();
-  saveToStorage()
-}
+    nameElement.value='';
+    renderTodoList();
+    saveToStorage()
+  
+  }
+  }
 
 function renderTodoList() {
   todoListHtml = '';
 
   for (let i=0; i<todoList.length; i++) {
     const name = todoList[i];
-    const html = `<div>${name}</div>
+    const html = `<div class="name">${name}</div>
     <button onclick="
     todoList.splice(${i},1)
     renderTodoList();
