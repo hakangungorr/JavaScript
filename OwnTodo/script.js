c0cc8dc034fa3fa21ac61dab8188d87ad7a03007
Hakan <mangacanmantan11@gmail.com>
2023-08-26 02:53:01 +0300

    finished course

--- OwnTodo/script.js
+++ OwnTodo/script.js
@@ -4,12 +4,27 @@ const todoList = JSON.parse(localStorage.getItem('todoList'))|| []
   renderTodoList();
 
 
-function pressEnter(event) {
+/* function pressEnter(event) {
   const key =  event.key
   if (key === 'Enter' ) 
   addTodo();
 
-}
+} */
+
+
+//while doing keydown use document.body.addEventlistener and no need class but while doing onclick document.querySelector.addEventListener
+document.body.addEventListener('keydown',(event)=>{
+  if (event.key === 'Enter')
+  addTodo();
+})
+
+addButton = document.querySelector('.add-todo-button');
+addButton.addEventListener('click',()=>{
+  addTodo();
+})
+
+
+
 
 
 function addTodo(){
@@ -36,18 +51,14 @@ function addTodo(){
 function renderTodoList() {
   let todoListHtml = '';
 
-  todoList.forEach(function(todoObject,index){
+  todoList.forEach((todoObject,index)=>{
     const name = todoObject.name;
     const date = todoObject.date;
 
     
     const html = `<div class="todo-item"><p class="name">${name}</p>
     <p class="date">${date}</p>
-    <button onclick="
-    todoList.splice(${index},1);
-    renderTodoList();
-    saveToStorage();
-    " class="delete-todo-button" 
+    <button class="delete-todo-button js-delete-todo-button" 
     >Delete</button></div>
     `;
     todoListHtml += html;
@@ -55,6 +66,14 @@ function renderTodoList() {
 
   document.querySelector('.js-todolist').innerHTML = todoListHtml
 }
+
+document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index)=>{
+deleteButton.addEventListener('click',()=>{
+    todoList.splice(index,1);
+    renderTodoList();
+    saveToStorage();
+})});  //all classes are chosen with queryselectorAll and this is like an array because of that we should use loop 
+
 
 
 
